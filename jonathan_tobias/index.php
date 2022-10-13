60a71eb68f54b6d1ca841fa0cf274a79db7eeb78
TOBY032 <114514364+TOBY032@users.noreply.github.com>
2022-10-13 09:58:02 -0500

    Add files via upload

---
+++ jonathan_tobias/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My first PHP page</h1>
+    
+    <?php 
+    ECHO "HELLO WORLD!!!!<br>";
+    echo "HELLO WORLD!!!!<br>";
+    EchO "HELLO WORLD!!!!<br>";
+    ?>
+
+    <?php 
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My car is " . $COLOR . "<br>";
+    echo "My car is " . $coLOR .  "<br>";
+    ?>
+
+    <?php
+    $var1 = 20;
+
+    var_dump($var1);
+    ?>
+
+</body>
+</html>
